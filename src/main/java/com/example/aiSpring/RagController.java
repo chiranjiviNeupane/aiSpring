2b5f1f071655a9ce44e09a93798e5c5537c47a40
chiranjiviNeupane <crucialjudge@gmail.com>
2026-09-05 11:29:04 +1000

    added Rag Controller and files for chunking and embeding

---
+++ src/main/java/com/example/aiSpring/RagController.java
@@ -0,0 +1,58 @@
+package com.example.aiSpring;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RagController {
+
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public RagController(ChatClient.Builder builder, VectorStore vectorStore){
+        this.chatClient = builder.build();
+        this.vectorStore = vectorStore;
+    }
+
+    // search the local vector embedding, then prompt gemini with embedding data
+    @PostMapping("/ask-docs")
+    public String askDocs(@RequestBody String question){
+        var qaAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
+                .searchRequest(SearchRequest.builder()
+                        .similarityThreshold(0.5)
+                        .topK(4)
+                        .build())
+                .build();
+
+        return chatClient.prompt()
+                .advisors(qaAdvisor)
+                .user(question)
+                .call()
+                .content();
+    }
+
+    //only for testing to see the difference between local embedding response and /ask-docs
+    @PostMapping("/search")
+    public List<String> search(@RequestBody String question){
+        List<Document> hits = vectorStore.similaritySearch(
+                SearchRequest.builder()
+                        .query(question)
+                        .similarityThreshold(0.0)
+                        .topK(4)
+                        .build());
+
+        return hits.stream()
+                .map(d -> "score=" + d.getScore()
+                + "file=" + d.getMetadata().get("filename")
+                + " :: " + d.getText().substring(0,Math.min(120,d.getText().length())))
+                .toList();
+    }
+}
